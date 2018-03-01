646c3dafe584f87283b7d60a4fab6ac650e1fc80
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-03-01 16:43:48 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int a,n,s,b;
+    clrscr();
+    printf("enter the values of a,n,s:");
+    scanf("%d%d%d",&a,&n,&s);
+    b=a+(n+2)*s;
+    printf("%d",b);
+    
+}
