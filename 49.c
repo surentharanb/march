54157390eb1618de672c8968d32e153456ee571b
surentharanb <35728331+surentharanb@users.noreply.github.com>
2018-03-03 14:46:58 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the values of a and b:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The sum of number is %d",c);
+    
+}
